83a7c692bf8db50fa55f16ef28c4a94e2816c631
Hannah Wolfe <erisds@gmail.com>
2018-06-21 13:54:31 +0100

    🎨 Cleanup default test env
    - we have no need for sinon

--- packages/nql-lang/test/utils/overrides.js
+++ packages/nql-lang/test/utils/overrides.js
@@ -4,7 +4,3 @@
 // Should is a global in our eslint test config
 global.should = require('should').noConflict();
 should.extend();
-
-// Sinon is a simple case
-// Sinon is a global in our eslint test config
-global.sinon = require('sinon');
