99c7f941ef6b508109c431af262e4ede0831bbc1
Hannah Wolfe <github.erisds@gmail.com>
2022-03-02 13:13:35 +0000

    Added 100% test coverage + enforced it
    - We were really close here to having 100% coverage so I've taken steps to get us there
    - -100 is the same as adding --check-coverage and setting all 4 types to 100

--- packages/nql/test/unit/api.test.js
+++ packages/nql/test/unit/api.test.js
@@ -2,7 +2,7 @@ require('../utils');
 
 const mingo = require('mingo');
 const nqlLang = require('@tryghost/nql-lang');
-const nql = require('../../lib/nql');
+const nql = require('../../');
 const knex = require('knex')({client: 'mysql'});
 const sandbox = sinon.createSandbox();
 
@@ -30,6 +30,10 @@ describe('Public API', function () {
     it('Basic API works as expected', function () {
         const query = nql('id:3');
 
+        query.lex().should.eql([
+            {token: 'PROP', matched: 'id:'},
+            {token: 'NUMBER', matched: '3'}
+        ]);
         query.toJSON().should.eql({id: 3});
         query.toString().should.eql('id:3');
 
@@ -45,6 +49,16 @@ describe('Public API', function () {
 
         query1.parse().should.eql({id: 3});
         query2.parse().should.eql({featured: true});
+
+        query1.lex().should.eql([
+            {token: 'PROP', matched: 'id:'},
+            {token: 'NUMBER', matched: '3'}
+        ]);
+
+        query2.lex().should.eql([
+            {token: 'PROP', matched: 'featured:'},
+            {token: 'TRUE', matched: 'true'}
+        ]);
 
         query1.queryJSON({id: 3}).should.be.true();
         query1.queryJSON({id: 2}).should.be.false();
@@ -91,6 +105,64 @@ describe('Public API', function () {
 
         query.queryJSON({tags: [{slug: 'video'}, {slug: 'audio'}]}).should.be.false();
         query.queryJSON({id: 3, order: 0, tags: [{slug: 'video'}, {slug: 'photo'}, {slug: 'audio'}]}).should.be.true();
+    });
+
+    it('Overrides perform as expected when no attempt to override', function () {
+        const query = nql('slug:hello-world', {overrides: 'status:published'});
+
+        query.toJSON().should.eql({$and: [
+            {status: 'published'},
+            {slug: 'hello-world'}
+        ]});
+
+        query.toString().should.eql('slug:hello-world');
+
+        query.queryJSON({slug: 'hello-world', status: 'draft'}).should.be.false();
+        query.queryJSON({slug: 'hello-world', status: 'published'}).should.be.true();
+    });
+
+    it('Overrides cannot be overriden', function () {
+        const query = nql('slug:hello-world+status:draft', {overrides: 'status:published'});
+
+        // @TODO: this could merge more cleanly..
+        query.toJSON().should.eql({$and: [
+            {status: 'published'},
+            {$and: [{slug: 'hello-world'}]}
+        ]});
+
+        query.toString().should.eql('slug:hello-world+status:draft');
+
+        query.queryJSON({slug: 'hello-world', status: 'draft'}).should.be.false();
+        query.queryJSON({slug: 'hello-world', status: 'published'}).should.be.true();
+    });
+
+    it('Defaults are added', function () {
+        const query = nql('slug:hello-world', {defaults: 'page:true'});
+
+        query.toJSON().should.eql({$and: [
+            {slug: 'hello-world'},
+            {page: true}
+        ]});
+
+        query.toString().should.eql('slug:hello-world');
+
+        query.queryJSON({slug: 'hello-world', page: true}).should.be.true();
+        query.queryJSON({slug: 'hello-world', page: false}).should.be.false();
+    });
+
+    it('Defaults can be overriden', function () {
+        const query = nql('slug:hello-world+page:false', {defaults: 'page:true'});
+
+        // @TODO: this could merge more cleanly..
+        query.toJSON().should.eql({$and: [
+            {slug: 'hello-world'},
+            {page: false}
+        ]});
+
+        query.toString().should.eql('slug:hello-world+page:false');
+
+        query.queryJSON({slug: 'hello-world', page: true}).should.be.false();
+        query.queryJSON({slug: 'hello-world', page: false}).should.be.true();
     });
 
     describe('parsing when options.transformer is set', function () {
